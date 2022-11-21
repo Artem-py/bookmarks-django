05aee01ea173434b8729ad7e1f759fb809d8f8e2
Artem Chervontsev <artyom.chervontsev@gmail.com>
2022-11-21 17:37:12 +0300

    Build a generic activity stream application
    Create a new application named actions. Add it to the apps list.
    Add an Action model using the contenttypes framework that can
    track all models installed in the project and also provides a
    generic interface to interact with models.

--- account/views.py
+++ account/views.py
@@ -9,6 +9,8 @@ from django.contrib.auth.decorators import login_required
 from .forms import LoginForm, UserRegistrationForm, \
                    UserEditForm, ProfileEditForm
 from .models import Profile, Contact
+from actions.utils import create_action
+from actions.models import Action
 
 
 def user_login(request):
@@ -34,7 +36,18 @@ def user_login(request):
 
 @login_required
 def dashboard(request):
-    return render(request, 'account/dashboard.html', {'section': 'dashboard'})
+    # display all actions by default
+    actions = Action.objects.exclude(user=request.user)
+    following_ids = request.user.following.values_list('id', flat=True)
+    if following_ids:
+        # If user following others, retrieve only their actions
+        actions = actions.filter(user_id__in=following_ids)
+    actions = actions.select_related('user', 'user__profile')\
+                     .prefetch_related('target')[:10]
+    return render(request,
+                  'account/dashboard.html', 
+                  {'section': 'dashboard',
+                  'actions': actions})
     
 
 def register(request):
@@ -48,6 +61,7 @@ def register(request):
             # Save the user object
             new_user.save()
             Profile.objects.create(user=new_user)
+            create_action(new_user, 'has created an account')
             return render(request, 'account/register_done.html', {'new_user': new_user})
     else:
         user_form = UserRegistrationForm()
@@ -107,6 +121,7 @@ def user_follow(request):
                     user_from=request.user,
                     user_to=user
                 )
+                create_action(request.user, 'is following', user)
             else:
                 Contact.objects.filter(user_from=request.user,
                                        user_to=user).delete()
